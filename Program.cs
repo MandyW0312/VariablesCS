9623373f42bf9ec0ff47f2eb6207bf2c382dda4c
Mandy Wade <lightninggirl413@hotmail.com>
2021-01-05 13:40:25 -0500

    Practice Getting Input From the User

--- Program.cs
+++ Program.cs
@@ -11,8 +11,12 @@ namespace VariablesCS
             var numberOfCupsOfCoffee = 1;
             var fullName = "Amanda Joy Wade";
             var today = DateTime.Now;
-
             Console.WriteLine(numberOfCupsOfCoffee + " " + fullName + " " + today);
+
+            Console.Write("What is your name? ");
+            var userName = Console.ReadLine();
+
+            Console.WriteLine("It is very nice to meet you " + userName);
 
 
         }
